9ec2bb367cc5703ccca96e89584316b941d8f8cf
Hawk9293 <zarex-jp@mail.ru>
2020-10-20 14:26:49 +0300

    Updates

---
+++ precache-manifest.3a785cf096c1613e833e38130c262921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5f4436cf0382b74de5c78fdd06e8df8",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "c0dc0a093f14efc87875",
+    "url": "/todo/static/css/main.4dccb335.chunk.css"
+  },
+  {
+    "revision": "d548c1277a4196cc48b8",
+    "url": "/todo/static/js/2.d3eee47a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/todo/static/js/2.d3eee47a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0dc0a093f14efc87875",
+    "url": "/todo/static/js/main.cb24287b.chunk.js"
+  },
+  {
+    "revision": "a03a00bd26cf7edb86b6",
+    "url": "/todo/static/js/runtime-main.8eb62d04.js"
+  }
+]);
